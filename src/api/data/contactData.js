00baf48211779dfaa0ab13124219073c434eeaca
Dario Perez <darioperez1415@gmail.com>
2022-01-07 17:15:29 -0600

    Added Api

--- src/api/data/contactData.js
+++ src/api/data/contactData.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
 import firebaseConfig from '../apiKeys';
-
 const dbUrl = firebaseConfig.databaseURL;
-
 const getContacts = (user) => new Promise((resolve, reject) => {
   axios
     .get(`${dbUrl}/eContacts.json?orderBy="uid"&equalTo="${user}"`)
     .then((response) => resolve(Object.values(response.data)))
     .catch(reject);
 });
-
+const getContactFB = (fbKey) => new Promise((resolve, reject) => {
+  axios
+    .get(`${dbUrl}/eContacts/${fbKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch(reject);
+});
 const createContact = (obj) => new Promise((resolve, reject) => {
   axios
     .post(`${dbUrl}/eContacts.json`, obj)
@@ -23,14 +26,12 @@ const createContact = (obj) => new Promise((resolve, reject) => {
     })
     .catch(reject);
 });
-
 const updateContact = (obj) => new Promise((resolve, reject) => {
   axios
     .patch(`${dbUrl}/eContacts/${obj.firebaseKey}.json`, obj)
     .then(() => getContacts(obj.uid).then(resolve))
     .catch(reject);
 });
-
 const deleteContact = (obj) => new Promise((resolve, reject) => {
   axios
     .delete(`${dbUrl}/eContacts/${obj.firebaseKey}.json`)
@@ -39,17 +40,10 @@ const deleteContact = (obj) => new Promise((resolve, reject) => {
     })
     .catch(reject);
 });
-
-const updateSingleContact = (firebaseKey) => new Promise((resolve, reject) => {
-  axios
-    .get(`${dbUrl}/eContacts/${firebaseKey}.json`)
-    .then((response) => resolve(response.data))
-    .catch(reject);
-});
 export {
   getContacts,
+  getContactFB,
   createContact,
   updateContact,
   deleteContact,
-  updateSingleContact,
 };
